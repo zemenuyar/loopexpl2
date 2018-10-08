d765ebc71526f90b79da4b17fffe17e7cd0ef6c8
zemenuyar <zemenuyar@gmail.com>
2018-10-08 13:29:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        for (int i= 10; i >= 0
+                ; i--){
+            System.out.println(i);
+        }
+
+    }
+}
